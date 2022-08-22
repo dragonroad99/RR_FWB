Octavi-OS/platform_frameworks_base

Base: PPU: Add Gmail to spoof list Another app blessed with monet compatibility.
Change-Id: Ibba8d72b92f809a0030956795a1fac4145ea9813

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -37,7 +37,7 @@ public class PixelPropsUtils {
             "com.google.android.apps.customization.pixel",
             "com.google.android.apps.fitness",
             "com.google.android.apps.gcs",
-            "com.google.android.apps.googleassistant",            
+            "com.google.android.apps.googleassistant",
             "com.google.android.apps.maps",
             "com.google.android.apps.messaging",
             "com.google.android.apps.nexuslauncher",
@@ -70,7 +70,8 @@ public class PixelPropsUtils {
             "com.google.pixel.dynamicwallpapers",
             "com.google.pixel.livewallpaper",
             "com.google.android.apps.photos",
-            "com.google.android.googlequicksearchbox"
+            "com.google.android.googlequicksearchbox",
+            "com.google.android.gm"
     };
 
     static {

dragonroad99/RR_FWB

Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
Signed-off-by: ghou8s <ghou8s@gmail.com>

core/java/com/android/internal/util/rr/PixelPropsUtils.java
@@ -50,7 +50,8 @@ public class PixelPropsUtils {
             "com.android.chrome",
             "com.android.vending",
             "com.breel.wallpapers20",
-     	    "com.google.android.apps.wallpaper"
+     	    "com.google.android.apps.wallpaper",
+            "com.google.android.gm"
     };
 
     private static ArrayList<String> allProps = new ArrayList<>(Arrays.asList("BRAND", "MANUFACTURER", "DEVICE", "PRODUCT", "MODEL", "FINGERPRINT"));